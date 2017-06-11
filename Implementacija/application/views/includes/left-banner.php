6e113c70c4dc5e40535f3df246c9c09afd5a1ca5
Igor <Igor>
2017-06-11 11:34:40 +0200

    Dohvatanje putanja reklama AJAX

--- Implementacija/application/views/includes/left-banner.php
+++ Implementacija/application/views/includes/left-banner.php
@@ -4,30 +4,28 @@
 
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Tajanstveni Deda Mraz</title>
-	<link href="css/bootstrap.css" rel="stylesheet">
-	<link href="css/mindCSS.css" rel="stylesheet">
-	<meta charset="UTF-8">
+	<script type="text/javascript">
+		$( document ).ready(function() {
+			$.ajax({url: "http://localhost:8080/ci/Reklamiranje/dohvatiSlikeOglasa/true", dataType: 'json', success: function(result){
+				console.log(result)
+		        for (var i = 0; i < result.length; i++) {
+		        	console.log(result[i]['PutanjaDoSlike'])
+			        $('#leftBanner').append('<img src="' + result[i]['PutanjaDoSlike'] + '">');
+			    }
+		    }});
 
-  	<meta name="viewport" content="width=device-width, initial-scale=1">
-  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		    $.ajax({url: "http://localhost:8080/ci/Reklamiranje/dohvatiSlikeOglasa/false", dataType: 'json', success: function(result){
+				console.log(result)
+		        for (var i = 0; i < result.length; i++) {
+		        	console.log(result[i]['PutanjaDoSlike'])
+			        $('#rightBanner').append('<img src="' + result[i]['PutanjaDoSlike'] + '">');
+			    }
+		    }});
+		});
+	</script>
 
-<style type="text/css">
-	body {
-		background: url('./slike/background.jpg');
-		color: #ffffff;
-	}
-</style>
-
-</head>
-<body>
 	<div class="col-xs-0 col-sm-0 col-md-2">
 		<div id="leftBanner" class="row banner">
-			<img src=<?php echo base_url() . "/slike/nonstopshop.png"?>>
-			<img src=<?php echo base_url() . "./slike/idealan.png"?>>
-			<img src=<?php echo base_url() . "./slike/ludPoklon.png"?>>
 		</div>
 	</div>
-</body>
+</html>
--- Implementacija/application/views/includes/right-banner.php
+++ Implementacija/application/views/includes/right-banner.php
@@ -4,31 +4,8 @@
 
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Tajanstveni Deda Mraz</title>
-
-	<link href="css/bootstrap.css" rel="stylesheet">
-	<link href="css/mindCSS.css" rel="stylesheet">
-	<meta charset="UTF-8">
-
-  	<meta name="viewport" content="width=device-width, initial-scale=1">
-  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-<style type="text/css">
-	body {
-		background: url('./slike/background.jpg');
-		color: #ffffff;
-	}
-</style>
-
-</head>
-<body>
 	<div class="col-xs-0 col-sm-0 col-md-2">
 		<div id="rightBanner" class="row banner">
-			<img src=<?php echo base_url() . "/slike/nonstopshop.png"?>>
-			<img src=<?php echo base_url() . "./slike/idealan.png"?>>
-			<img src=<?php echo base_url() . "./slike/ludPoklon.png"?>>
 		</div>
 	</div>
-</body>
+</html>
